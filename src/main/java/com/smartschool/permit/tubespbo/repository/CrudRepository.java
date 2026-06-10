5d1e98a5fa548ef87081ba1a618661030446c9d7
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2026-06-10 16:18:02 +0700

    CRUD Repo

---
+++ src/main/java/com/smartschool/permit/tubespbo/repository/CrudRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.smartschool.permit.tubespbo.repository;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Doni354
+ */
+public interface CrudRepository {
+    List<T> getAll();
+    T getById(String id);
+    String create(T entity);
+    void update(String id, Map<String, Object> data);
+    void delete(String id);
+}
