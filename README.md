8bbea50367306f720e8a9b3b71f112b5fc6cb4f8
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2026-06-10 15:41:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TubesPBO
+Tubes PBO 
---
+++ src/main/java/com/smartschool/permit/tubespbo/TubesPBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.smartschool.permit.tubespbo;
+
+/**
+ *
+ * @author Doni354
+ */
+public class TubesPBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
