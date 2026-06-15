4bf61039f27088eae1f4e754478013237a1e2331
RNames <124018774+RNames@users.noreply.github.com>
2026-06-15 15:38:43 +0700

    GUI

---
+++ src/main/java/com/smartschool/permit/tubespbo/gui/dashboard/BlankPanel.java
@@ -0,0 +1,17 @@
+package com.mycompany.gui.dashboard;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BlankPanel extends JPanel {
+
+    public BlankPanel(String menuName) {
+        setLayout(new BorderLayout());
+        
+        JLabel label = new JLabel("Halaman " + menuName + " Belum Diimplementasikan", SwingConstants.CENTER);
+        label.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+        label.setForeground(Color.GRAY);
+        
+        add(label, BorderLayout.CENTER);
+    }
+}
---
+++ src/main/java/com/smartschool/permit/tubespbo/gui/login/Main.java
@@ -0,0 +1,7 @@
+package com.mycompany.gui.login;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginFrame.main(args);
+    }
+}
